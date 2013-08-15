e3d09a1b30fdd3b7d19a39668fc261e814b8ffcb
Ming Xiao <mingxiao10016@gmail.com>
2013-08-15 11:20:42 -0700

    Added UserToken db model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import dropbox
 import data
 import logging
+from userToken import UserToken
 
 jinja_env = jinja2.Environment(autoescape=True, 
                                        loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'templates')))
@@ -14,16 +15,17 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         template = jinja_env.get_template('index.html')
-#        self.response.out.write(template.render())
-        sess = dropbox.session.DropboxSession(data.app_key,data.app_secret)
-        request_token = sess.obtain_request_token()
-        logging.info(request_token.key+','+request_token.secret)
-        self.response.out.write(request_token.key+','+request_token.secret)
-        callback_url = self.request.url + 'callback?oauth_secret={}'.format(request_token.secret)
-        auth_url = sess.build_authorize_url(request_token, callback_url)
-        self.redirect(auth_url)
+        self.response.out.write(template.render())
+#        sess = dropbox.session.DropboxSession(data.app_key,data.app_secret)
+#        request_token = sess.obtain_request_token()
+#        logging.info(request_token.key+','+request_token.secret)
+#        self.response.out.write(request_token.key+','+request_token.secret)
+#        callback_url = self.request.url + 'callback?oauth_secret={}'.format(request_token.secret)
+#        auth_url = sess.build_authorize_url(request_token, callback_url)
+#        self.redirect(auth_url)
         
     def post(self):
+        user = self.request.get('user')
         pass
         
 
